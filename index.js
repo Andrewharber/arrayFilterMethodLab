dbdb0186c4147c61b547c972c1fa26ff51af9111
Andrewharber <harber.andrew@ymail.com>
2022-09-20 14:48:54 -0400

    commit tree 9a92bd2e04ad2f4cca37a442e6430b8286d856fe

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
+const { stringMatching } = require("expect")
+
 // Code your solution here
+//const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']
+
+
+function findMatching(drivers,string){
+    let matchingDrivers = drivers.filter(element => element === string || element === string.toLowerCase())
+    return matchingDrivers
+}
+
+function fuzzyMatch(drivers,string){
+    let stringCheck = drivers.filter(drivers => drivers.slice(0, string.length) === string)
+    return stringCheck
+}
+
+const drivers = [
+    {
+      name: 'Bobby',
+      hometown: 'Pittsburgh' },
+    {
+      name: 'Sammy',
+      hometown: 'New York' } ,
+    {
+      name: 'Sally',
+      hometown: 'Cleveland' },
+    {
+      name: 'Annette',
+      hometown: 'Los Angeles' },
+    {
+      name: 'Bobby',
+      hometown: 'Tampa Bay' }
+  ];
+
+ function matchName(driver,string){
+     return driver.filter((driver) => driver.name === string)
+ }
